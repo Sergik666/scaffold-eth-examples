51e11b6b4db03676e13de4a49c0adffa9d2439d3
Sergii Vasylchenko <sergik666@gmail.com>
2022-09-12 12:55:31 +0300

    Added create wallet modal component

---
+++ packages/react-app/src/components/CreateWalletModal.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { Button, Modal, Input } from "antd";
+import AddressInput from "./AddressInput";
+
+export default function CreateWalletModal({ visible, handleOk, mainnetProvider, createWallet }) {
+
+  const [wallets, setWallets] = useState(['']);
+  const [signaturesRequired, setSignaturesRequired] = useState(1);
+
+  return (
+    <Modal
+      title="Create Wallet"
+      visible={visible}
+      onCancel={handleOk}
+      destroyOnClose
+      onOk={handleOk}
+      footer={null}
+      closable
+      maskClosable
+    >
+      <div style={{ margin: 8, padding: 8 }}>
+        Owners:
+      </div>
+      {wallets.map((wallet, index) =>
+        <div style={{ margin: 8, padding: 8, display: 'flex' }}>
+          <div style={{ flexGrow: 1 }}>
+            <AddressInput
+              autoFocus
+              ensProvider={mainnetProvider}
+              placeholder="new owner address"
+              style={{ flexGrow: 1 }}
+              value={wallet}
+              onChange={(address) => {
+                const newWallets = [...wallets];
+                newWallets[index] = address;
+                setWallets(newWallets);
+              }}
+            />
+          </div>
+          <Button
+            style={{ marginLeft: 5 }}
+            disabled={wallets.length < 2}
+            onClick={() => {
+              setWallets([
+                ...wallets.slice(0, index),
+                ...wallets.slice(index + 1)
+              ]);
+            }}
+          >x</Button>
+        </div>
+      )}
+      <div style={{ margin: 8, padding: 8, display: 'flex' }}>
+        <Button
+          style={{ flexGrow: 1 }}
+          onClick={() => {
+            setWallets([...wallets, '']);
+          }}
+        >+</Button>
+      </div>
+      <div style={{ margin: 8, padding: 8 }}>
+        Count of signatures required:
+      </div>
+      <div style={{ margin: 8, padding: 8 }}>
+        <Input
+          ensProvider={mainnetProvider}
+          placeholder="signatures"
+          value={signaturesRequired}
+          onChange={(e) => { setSignaturesRequired(e.target.value) }}
+        />
+      </div>
+      <div style={{ margin: 8, padding: 8, display: 'flex' }}>
+        <Button
+          style={{ flexGrow: 1 }}
+          onClick={() => {
+            createWallet(wallets, signaturesRequired);
+          }}
+        >Create</Button>
+      </div>
+    </Modal>
+  );
+};
+
